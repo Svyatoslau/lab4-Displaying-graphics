1d6d811eed89eedab3af5360a265659539342952
Svyatoslav Yaroshevich <svyatyar2@gmail.com>
2020-11-19 19:37:30 +0300

    MOD:graphicsStroke

--- src/bsu/rfe/java/group10/lab4/Yaroshevich/varC2/GraphicsDisplay.java
+++ src/bsu/rfe/java/group10/lab4/Yaroshevich/varC2/GraphicsDisplay.java
@@ -37,7 +37,7 @@ public class GraphicsDisplay extends JPanel {
         // Сконструировать необходимые объекты, используемые в рисовании
         // Перо для рисования графика
         graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
-                BasicStroke.JOIN_ROUND,10.0f,null,0.0f);
+                BasicStroke.JOIN_ROUND,10.0f,new float[]{3,1,1,1,2,1,1,1,3},0.0f);
         // Перо для рисования осей координат
         axisStroke = new BasicStroke(1.0f,BasicStroke.CAP_BUTT,
                 BasicStroke.JOIN_MITER,10.0f,null,0.0f);
